ef172c58174679ba6e6b11779756126881628e80
Andrew Martin <codingMartinPro@gmail.com>
2019-10-10 01:24:23 -0400

    added homework

--- Consolidated/src/com/armco/amarted/homework/Room/Lamp.java
+++ Consolidated/src/com/armco/amarted/homework/Room/Lamp.java
@@ -1,4 +1,30 @@
 package com.armco.amarted.homework.Room;
 
 public class Lamp {
+    private String style;
+    private boolean battery;
+    private int gloRating;
+
+    public Lamp(String style, boolean battery, int gloRating) {
+        this.style = style;
+        this.battery = battery;
+        this.gloRating = gloRating;
+    }
+
+    public void turnOn(){
+        System.out.println("Turning on lamp...");
+    }
+
+
+    public String getStyle() {
+        return style;
+    }
+
+    public boolean isBattery() {
+        return battery;
+    }
+
+    public int getGloRating() {
+        return gloRating;
+    }
 }
--- Consolidated/src/com/armco/amarted/homework/Room/Wall.java
+++ Consolidated/src/com/armco/amarted/homework/Room/Wall.java
@@ -1,4 +1,13 @@
 package com.armco.amarted.homework.Room;
 
 public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
 }
