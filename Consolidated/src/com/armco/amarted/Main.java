6a3de0fa5f843ede324336812797a30461cfb47a
Andrew Martin <codingMartinPro@gmail.com>
2019-10-26 21:00:15 -0400

    array info

--- Consolidated/src/com/armco/amarted/Main.java
+++ Consolidated/src/com/armco/amarted/Main.java
@@ -1,12 +1,17 @@
 package com.armco.amarted;
 
 import com.armco.amarted.homework.Homework;
+import com.armco.amarted.homework.Room.*;
 import com.armco.amarted.notes.*;
 import com.armco.amarted.notes.ClassesMethods.CarMain;
 import com.armco.amarted.notes.ClassesMethods.Constructors.BankAccount;
 import com.armco.amarted.notes.ClassesMethods.Constructors.VipCustomer;
+import com.armco.amarted.notes.Composition.*;
+import com.armco.amarted.notes.Encapsulation.*;
+import com.armco.amarted.notes.Inheritance.AgentCar;
+import com.armco.amarted.notes.Inheritance.Car;
 import com.armco.amarted.notes.Inheritance.InheritanceMain;
-import jdk.swing.interop.SwingInterOpUtils;
+import com.armco.amarted.notes.Inheritance.Vehicle;
 
 public class Main {
 
@@ -129,8 +134,86 @@ public class Main {
         System.out.println("*** ACTIVATING ANIMALS ***");
         InheritanceMain.callMain();
 
+        System.out.println("\nCALLING UPON VEHICLE INHERITANCES");
+//        Vehicle monorail = new Vehicle(0,110);
+//        Car toyotaSedan = new Car(40,1);
+//        BondCar agent007 = new BondCar(80,-1,false,"Mini-Rockets");
+//        System.out.println("Monorail Speed: " + monorail.getSpeed());
+//        System.out.println("Sedan Speed: " + toyotaSedan.getSpeed());
+//        System.out.println("Bond's Speed: " + agent007.getSpeed());
+//        agent007.setSpeed(90);
+//        System.out.println("Bond's NEW Speed: " + agent007.getSpeed());
+        Vehicle excellaTrain = new Vehicle("ExcellaTrain","Modular");
+        Car honda = new Car("Accord","Asian",4,4,5,true);
+        AgentCar agent007 = new AgentCar("Mini-Rockets",false);
+//        excellaTrain.stop();
+//        honda.stop();
+//        agent007.stop();
+//        honda.changeVelocity(5,90);
+        agent007.activateWeapon();
+        agent007.steer(45);
+        agent007.accelerate(12);
+        agent007.accelerate(23);
+        agent007.accelerate(-30);
 
+        System.out.println("\n         ----- COMPOSITION over INHERITANCE-----");
+        // Composition Example
+        System.out.println("\n- SHOWING EXAMPLE OF COMPOSITION -");
+        Dimensions myDimensions = new Dimensions(20, 20, 5);
+        Case myCase = new Case("220B", "Dell", "240", myDimensions);
+        Monitor myMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
+        //                           create an instance of a class without making a variable ---^   by 'passing' a "new" in the parameters
+        Motherboard myMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
 
+        PC thePC = new PC(myCase,myMonitor,myMotherboard);
+//        thePC.getTheCase().pressPowerButton();
+//        thePC.getMotherboard().loadProgram("Start OS: Ubuntu 18.04");
+//        thePC.getMonitor().drawPixelAt(1500,1200,"red");
+        thePC.powerUp();
+
+        System.out.println("\nMaking a bedroom");
+        Wall wall1 = new Wall("North");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("West");
+        Ceiling ceiling = new Ceiling(8,5);
+        Bed bed = new Bed("Murphy",2,36,1000,2);
+        Lamp myLamp = new Lamp("Gooseneck",false,60);
+        Bedroom bedRoom = new Bedroom("Tim's",wall1,wall2,wall3,wall4,ceiling,bed,myLamp);
+        bedRoom.getLamp().turnOn();
+        bedRoom.makeBed();
+
+        System.out.println("\n         ----- EXAMPLE(S) OF ENCAPSULATION-----");
+        // ENCAPSULATION
+        System.out.println("\nEXAMPLE(S) OF ENCAPSULATION");
+        Player player1 = new Player();
+        player1.name = "Tim";
+        player1.health = 20;
+        player1.weapon = "Sword";
+        int damage = 10;
+        player1.loseHealth(damage);
+        System.out.println(player1.name + " took " + damage + " damage\nRemaining Health = " + player1.healthRemaining());
+        damage = 11;
+        player1.loseHealth(damage);
+        System.out.println(player1.name + " took " + damage + " damage\nRemaining Health = " + player1.healthRemaining());
+        // Proper Usage of Encapsulation Below
+        EnhancedPlayer player2 = new EnhancedPlayer("Tom",150,"Bow");
+        System.out.println("Initial health for " + player2.getName() + " is " + player2.getHealth());
+        player2.loseHealth(30);
+        // My 'Homework' on Encapsulation
+        System.out.println("- PRINTER HOMEWORK -");
+        Toner myToner = new Toner(100);
+        Printer printer1 = new Printer(true,300,5);
+        printer1.print();
+        printer1.print(4);
+        printer1.print(25);
+        printer1.addToner(40);
+        Printer printer2 = new Printer(false,500,1);
+        printer2.print(125);
+        printer2.addToner(50);
+        System.out.println(printer2.getToner());
+        System.out.println(printer1.isDuplex());
+        System.out.println(printer2.isDuplex());
 
 
 
